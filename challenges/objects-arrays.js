bd79f2b3b5ca61330a28803bee1dfa8287bb1255
Daniel Jones <daniel.jones2000@gmail.com>
2019-07-26 08:10:22 -0700

    created dino objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,11 +6,33 @@
   object name, diet, weight, length, period
 */
 
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+  'name': 'Tyrannosaurus',
+  'diet' : 'Carnivorous',
+  'weight' : '7000kg',
+  'length' : '12m',
+  'period' : 'Late Cretaceous'
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  'name': 'Stegosaurus',
+  'diet' : 'Herbivorous',
+  'weight' : '2000kg',
+  'length' : '9m',
+  'period' : 'Late Jurassic'
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = {
+  'name': 'Tyrannosaurus',
+  'diet' : 'Carnivorous',
+  'weight' : '15kg',
+  'length' : '1.8m',
+  'period' : 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
