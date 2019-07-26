1b2a86cb8863386b4ac2416538aa34ace1ffff7c
Daniel Jones <daniel.jones2000@gmail.com>
2019-07-26 08:18:54 -0700

    finished objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -27,7 +27,7 @@ const stegosaurus = {
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 const velociraptor = {
-  'name': 'Tyrannosaurus',
+  'name': 'Velociraptor',
   'diet' : 'Carnivorous',
   'weight' : '15kg',
   'length' : '1.8m',
@@ -37,22 +37,24 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`The Tyrannosaurus weighs ${tyrannosaurus.weight}.`);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`The diet of a velociraptor is ${velociraptor.diet}`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`A Stegosaurus's length is ${stegosaurus.length}.`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(`The Tyrannosaurus lived in the ${tyrannosaurus.period}.`);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
 
-
+tyrannosaurus.roar = function (){
+  return(`${this.name} says RAWERSRARARWERSARARARRRR!`);
+}
+console.log(tyrannosaurus.roar());
 // ==== Arrays ====
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
